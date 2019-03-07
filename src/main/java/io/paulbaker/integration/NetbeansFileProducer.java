8aadc0d5fd78b51b122ad19632521229ea5dc1cc
Paul N. Baker <paul.nelson.baker@gmail.com>
2019-03-07 07:43:01 -0700

    New files

---
+++ src/main/java/io/paulbaker/integration/NetbeansFileProducer.java
@@ -0,0 +1,24 @@
+package io.paulbaker.integration;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+
+/**
+ * @author Paul Nelson Baker
+ * @author WGU Student Info: pbake22 - #000412290
+ * @see <a href="https://github.com/paul-nelson-baker/">GitHub</a>
+ * @see <a href="https://www.linkedin.com/in/paul-n-baker/">LinkedIn</a>
+ * @since 2019-03
+ */
+@Mojo(name = "generate-ide-files")
+public class NetbeansFileProducer extends AbstractMojo {
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+
+    }
+
+
+}
---
+++ src/main/java/io/paulbaker/integration/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * @author Paul Nelson Baker
+ * @author WGU Student Info: pbake22 - #000412290
+ * @see <a href="https://github.com/paul-nelson-baker/">GitHub</a>
+ * @see <a href="https://www.linkedin.com/in/paul-n-baker/">LinkedIn</a>
+ * @since 2019-03
+ */
+package io.paulbaker.integration;
